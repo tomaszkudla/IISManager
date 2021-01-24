a335015a12dc0232fbcfa6f7800360941293b0bd
Tomasz Kudła <tomaszjankudla@gmail.com>
2021-01-24 19:09:21 +0100

    Getting applications for app pools - backend init

---
+++ IISManager/ViewModels/Application.cs
@@ -0,0 +1,59 @@
+﻿using System.ComponentModel;
+
+namespace IISManager.ViewModels
+{
+    public class Application : INotifyPropertyChanged
+    {
+        private string applicationPoolName;
+        private string dirPath;
+        private string webPath;
+
+        public Application(string applicationPoolName, string dirPath, string webPath)
+        {
+            this.applicationPoolName = applicationPoolName;
+            this.dirPath = dirPath;
+            this.webPath = webPath;
+        }
+
+        public string ApplicationPoolName
+        {
+            get { return applicationPoolName; }
+            set
+            {
+                if (this.applicationPoolName != value)
+                {
+                    this.applicationPoolName = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("ApplicationPoolName"));
+                }
+            }
+        }
+
+        public string DirPath
+        {
+            get { return dirPath; }
+            set
+            {
+                if (this.dirPath != value)
+                {
+                    this.dirPath = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("DirPath"));
+                }
+            }
+        }
+
+        public string WebPath
+        {
+            get { return webPath; }
+            set
+            {
+                if (this.webPath != value)
+                {
+                    this.webPath = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("WebPath"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ IISManager/ViewModels/ApplicationsList.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace IISManager.ViewModels
+{
+    public class ApplicationsList : INotifyPropertyChanged
+    {
+        private List<Application> value;
+
+        public ApplicationsList(List<Application> applications)
+        {
+
+            value = applications;    
+        }
+
+        public List<Application> Value
+        {
+            get { return value; }
+            set
+            {
+                if (this.value.Count == value.Count)
+                {
+                    for (int i = 0; i < value.Count; i++)
+                    {
+                        var currentValue = this.value[i];
+                        var newValue = value[i];
+
+                        currentValue.ApplicationPoolName = newValue.ApplicationPoolName;
+                        currentValue.DirPath = newValue.DirPath;
+                        currentValue.WebPath = newValue.WebPath;
+                    }
+                }
+                else
+                {
+                    this.value = value;
+                    PropertyChanged(this, new PropertyChangedEventArgs("Value"));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
